7df3a34a91aa01de1099405153065c86bc6e1717
Harinarayanan <harinarayananparideveloper@gmail.com>
2023-08-25 09:57:46 +0530

    first commit

---
+++ barber.js
@@ -0,0 +1,18 @@
+let menuBtn = document.getElementById("menu-btn")
+let sideNav = document.getElementById("sidenav")
+let menu = document.getElementById("menu")
+sideNav.style.right == "-250px"
+menuBtn.onclick = function(){
+    if (sideNav.style.right == "-250px"){
+            sideNav.style.right = "0";
+            menu.src = "images/close.png";
+        }
+    else{
+        sideNav.style.right = "-250px";
+        menu.src = "images/menu.png";
+        }
+    }
+            // var scroll = new SmoothScroll('a[href*="#"]', {
+            // speed: 1000,
+	        // speedAsDuration: true
+// });
